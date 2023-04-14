f1ab488be2a6ec236da997696789b94fd3f976c2
Amirah Parman <fthnamirah@hotmail.com>
2023-04-14 12:56:06 +0200

    Added calculations onbur

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 const distanceFormula = 1.2;
 
-
-let idealDistance = 0;
-let distanceCaculator = (idealDistance / distanceFormula); //to calculate distance 
-console.log('distance: ', distanceCaculator);
-
-// multiply the screen size by 1.2
+//to calculate TV size
 function tvSizeCalculator(idealDistance) {
     return idealDistance / distanceFormula;
 }
 
-function calculate() {
-    let sofaInputTextBox = document.getElementById("SofaInput");
-    let sofaDistance = sofaInputTextBox.value;
-
-    let tvSize = tvSizeCalculator(sofaDistance);
-
-    console.log('tv size should be: ', tvSize);
-
-    document.getElementById("tvSizeInput").value= tvSize;
+//to calculate distance
+function distanceLengthCalculator(idealSize){
+    return idealSize * distanceFormula;
 }
 
+function calculatedTv(){
+    let sofaInputTextBox = document.getElementById("SofaInput");
+    let sofaDistance = sofaInputTextBox.value;
+    
+    let tvSize = tvSizeCalculator(sofaDistance);
+    
+    console.log('tv size should be: ', tvSize);
+    
+    document.getElementById("tvSizeInput").value= tvSize;   
+}
 
+function calculatedDistance(){
+    let tvInputTextBox= document.getElementById("tvSizeInput");
+    let tvSizeInserted = tvInputTextBox.value;
+
+    let distanceSofa = distanceLengthCalculator(tvSizeInserted);
+    console.log('ideal distance should be:', distanceSofa);
+    
+    document.getElementById("SofaInput").value= distanceSofa;
+}
