ff71bbdc7dc320147d5624a2d9fb7c33c1f4592a
Amirah Parman <fthnamirah@hotmail.com>
2023-04-14 13:39:14 +0200

    add conversion and precision

--- script.js
+++ script.js
@@ -10,17 +10,25 @@ function distanceLengthCalculator(idealSize){
     return idealSize * distanceFormula;
 }
 
+function convertMetersToInches(tvSize) {
+    return tvSize * 39.37;
+  }
+
 function calculatedTv(){
     let sofaInputTextBox = document.getElementById("SofaInput");
     let sofaDistance = sofaInputTextBox.value;
     
     let tvSize = tvSizeCalculator(sofaDistance);
+
+    let convertedTvSize= convertMetersToInches(tvSize);
     
-    console.log('tv size should be: ', tvSize);
-    
-    document.getElementById("tvSizeInput").value= tvSize;   
+    document.getElementById("tvSizeInput").value= Math.round(convertedTvSize);   
 }
 
+function convertInchToMeter(distanceSofa){
+    return distanceSofa * 0.0254;
+}
+  
 function calculatedDistance(){
     let tvInputTextBox= document.getElementById("tvSizeInput");
     let tvSizeInserted = tvInputTextBox.value;
@@ -28,5 +36,8 @@ function calculatedDistance(){
     let distanceSofa = distanceLengthCalculator(tvSizeInserted);
     console.log('ideal distance should be:', distanceSofa);
     
-    document.getElementById("SofaInput").value= distanceSofa;
+    let ConvertedSofaDistance = convertInchToMeter(distanceSofa);
+
+    document.getElementById("SofaInput").value= ConvertedSofaDistance.toFixed(2);   
 }
+
